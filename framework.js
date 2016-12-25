1e00f635e09725d1873ee6c31dcf237588a24826
Jim Simon <jim.j.simon@gmail.com>
2016-12-24 21:51:23 -0500

    Added missing mocha functions for framework

--- framework.js
+++ framework.js
@@ -1,3 +1,21 @@
+// BDD or QUnit
+window.before = parent.before;
+window.after = parent.after;
+window.beforeEach = parent.beforeEach;
+window.afterEach = parent.afterEach;
+window.run = parent.run;
+
+// BDD
 window.describe = parent.describe;
 window.it = parent.it;
 window.xit = parent.xit;
+
+// QUnit
+window.suite = parent.suite;
+window.test = parent.test;
+
+// TDD
+window.setup = parent.setup;
+window.teardown = parent.teardown;
+window.suiteSetup = parent.suiteSetup;
+window.suiteTeardown = parent.suiteTeardown;
