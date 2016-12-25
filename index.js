8bc8d035476e4d88be8319c975ea31d861137e65
Jim Simon <jim.j.simon@gmail.com>
2016-12-25 17:02:04 -0500

    Implemented proxies

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var fs = require('fs');
 
 var pattern = function (file, included) {
     return {pattern: file, included: included, served: true, watched: false};
@@ -9,16 +10,38 @@ var framework = function (files) {
     files.push(pattern(path.join(__dirname, 'runner.js'), true));
 };
 
-var proxyBowerComponentsMiddlewareFactory = function () {
+var createProxyForDirectory = function (directory) {
     return function (request, response, next) {
-        next();
+        var filePath = request.url.replace('/base/', '');
+        console.log('Fragment: ' + filePath);
+        fs.exists(filePath, function (exists) {
+            if (exists) { //File exists at root, let karma attempt to serve it.
+                next();
+            } else {
+                filePath = path.join(directory, request.url.replace(/base\/|node_modules\//g, ''));
+                fs.exists(filePath, function (exists) {
+                    if (exists) { //Serve from bower_components
+                        // Content-type is very interesting part that guarantee that
+                        // Web browser will handle response in an appropriate manner.
+                        response.writeHead(200, {
+                            "Content-Type": "application/octet-stream",
+                            "Content-Disposition" : "attachment; filename=" + path.basename(filePath)});
+                        fs.createReadStream(filePath).pipe(response);
+                    } else { //Let karma serve the 404
+                        next();
+                    }
+                });
+            }
+        });
     };
 };
 
+var proxyBowerComponentsMiddlewareFactory = function () {
+    return createProxyForDirectory('bower_components');
+};
+
 var proxyNodeModulesMiddlewareFactory = function () {
-    return function (request, response, next) {
-        next();
-    };
+    return createProxyForDirectory('node_modules');
 };
 
 framework.$inject = ['config.files'];
